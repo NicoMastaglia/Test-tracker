8792d263f5465b38364d88807d94b339ed0de0f0
Nicola Mastaglia <nicomastaglia@gmail.com>
2026-05-08 17:00:24 +0200

    Auth middleware

---
+++ src/middleware/checkSuperadmin.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const checkSuperadmin = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .json({ message: "Token mancante o formato non valido" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (decoded.role === "superadmin") {
+      req.user = decoded;
+      next();
+    } else {
+      return res.status(403).json({ message: "Accesso negato" });
+    }
+  } catch (error) {
+    return res.status(401).json({ message: "Token non valido" });
+  }
+};
+
+module.exports = checkSuperadmin;
